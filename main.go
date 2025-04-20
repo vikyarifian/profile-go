8daa8369356a8b9b6f7b7f38f23a953d0e0c11e2
vikyarifian <vikyarifiansyah@gmail.com>
2025-04-20 22:36:26 +0700

    update-porto

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"log"
 	"net"
 	"os"
+	"path/filepath"
 	"profile-go/routes"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -12,13 +14,34 @@ import (
 )
 
 func LoadEnv() {
-	err := godotenv.Load(".env")
+	appPath, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal(err)
+	}
+
+	appPath = strings.ReplaceAll(appPath, "\\bin", "")
+	appPath = strings.ReplaceAll(appPath, "\\main", "")
+	appPath = strings.ReplaceAll(appPath, "/bin", "")
+	appPath = strings.ReplaceAll(appPath, "/main", "")
+	appPath = strings.ReplaceAll(appPath, "\\", "/")
+
+	os.Setenv("APP_PATH", appPath+"/")
+
+	err = godotenv.Load(appPath + ".env")
+	if err != nil {
+		err = godotenv.Load(appPath + "/.env")
+		if err != nil {
+			err = godotenv.Load(appPath + "\\.env")
+			if err != nil {
+				log.Fatal("Error loading .env file path: ", appPath)
+			}
+		}
 	}
 }
 
 func main() {
+
+	LoadEnv()
 
 	app := fiber.New(fiber.Config{
 		Network: fiber.NetworkTCP,
